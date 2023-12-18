2e02acea0c8971e2a5cd6e41de8f4adf178b0337
V4SYA <62791870+V4SYA@users.noreply.github.com>
2023-12-19 01:50:18 +0700

    Add files via upload

--- autorace_core_vvv/setup.py
+++ autorace_core_vvv/setup.py
@@ -4,27 +4,32 @@ from setuptools import find_packages, setup
 
 package_name = 'autorace_core_vvv'
 
+
+
 setup(
     name=package_name,
     version='0.0.0',
     packages=find_packages(exclude=['test']),
     data_files=[
-        ('share/ament_index/resource_index/packages',
-            ['resource/' + package_name]),
-        ('share/' + package_name, ['package.xml']),
-        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
-    ],
-    install_requires=['setuptools'],
+     ('share/ament_index/resource_index/packages',
+             ['resource/' + package_name]),
+     ('share/' + package_name, ['package.xml']),
+     (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*.launch.py'))),
+     (os.path.join('share', package_name, 'calibration'), glob('calibration/*'))
+   ],
+    install_requires=['setuptools',
+                      'ultralytics',],
     zip_safe=True,
-    maintainer='Vasya',
+    maintainer='Vasta',
     maintainer_email='v.postnykh@g.nsu.ru',
-    description='Contains launch files for the robot project',
-    license='Apache 2.0',
+    description='TODO: Package description',
+    license='TODO: License declaration',
     tests_require=['pytest'],
     entry_points={
         'console_scripts': [
-        'detect_line=autorace_core_vvv.detect_line:main',
-        'pid=autorace_core_vvv.pid:main'
+            'path_tracking=autorace_core_rossiane.path_tracking:main',
+            'take_picture=autorace_core_rossiane.take_picture:main',
+            'pid_controller=autorace_core_rossiane.pid_controller:main'
         ],
     },
 )
